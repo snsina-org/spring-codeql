162aaadf162c8364d285e57c19d52445cfae71f0
snsina <sn@sns-macbook-pro.mynetworksettings.com>
2022-10-17 21:22:21 -0400

    initiate spring boot

---
+++ src/main/java/org/snsina/codeql/CodeqlApplication.java
@@ -0,0 +1,13 @@
+package org.snsina.codeql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/snsina/codeql/CodeqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.snsina.codeql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
